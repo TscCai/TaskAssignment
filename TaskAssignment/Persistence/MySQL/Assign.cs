76d3f676d6ba86bcfe04952b2a5dfd59d1644c3a
TscCai <ohyeahsohot@163.com>
2018-01-14 17:14:13 +0800

    Inital submit, SQLite with EF6 works fine.

---
+++ TaskAssignment/Persistence/MySQL/Assign.cs
@@ -0,0 +1,13 @@
+﻿// Code may be different between EF6-SQLite and EF6-MySQL
+// Use compiler directive to control different dialect
+#if MYSQL
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskAssignment.Persistence {
+    public partial class Assign {
+    }
+}
+#endif
---
+++ TaskAssignment/Persistence/SQLite/Member.cs
@@ -0,0 +1,34 @@
+// Code may be different between EF6-SQLite and EF6-MySQL
+// Use compiler directive to control different dialect
+#if SQLITE
+namespace TaskAssignment.Persistence
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Member
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Member()
+        {
+            Assigns = new HashSet<Assign>();
+        }
+
+        public long Id { get; set; }
+
+        [Required]
+        [StringLength(2147483647)]
+        public string Name { get; set; }
+
+        public bool Enable { get; set; }
+
+        public bool IsInternal { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Assign> Assigns { get; set; }
+    }
+}
+#endif
