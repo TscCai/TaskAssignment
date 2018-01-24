1cb6156ba854acd35ebb6d818d9f9f614ad148a1
TscCai <caijiran@hotmail.com>
2018-01-24 21:45:56 +0800

    a single task can be added into db now.

--- TaskAssignment/Controllers/AdminController.cs
+++ TaskAssignment/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TaskAssignment.Persistence;
+using TaskAssignment.Models;
 using TaskAssignment.Util;
 
 namespace TaskAssignment.Controllers
@@ -26,8 +27,29 @@ namespace TaskAssignment.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddTask(Task model) {
+        public ActionResult AddTask(AddTask model) {
+            Task t = model.Task;
+            var ctx = new TaskAssignmentModel();
+            ctx.Tasks.Add(t);
             
+            if (model.LeaderId > 0) {
+                Assign leader = new Assign();
+                leader.MemberId = model.LeaderId;
+                leader.IsLeader = true;
+                leader.TaskId = t.Id;
+                ctx.Assigns.Add(leader);
+            }
+
+            foreach (var item in model.MemberId) {
+                Assign asg = new Assign();
+                asg.MemberId = item;
+                asg.IsLeader = false;
+                asg.TaskId = t.Id;
+                ctx.Assigns.Add(asg);
+            }
+            ctx.SaveChanges();
+
+            ViewBag.Success = true;
             return View();
         }
 
@@ -41,7 +63,7 @@ namespace TaskAssignment.Controllers
             var tasks = ctx.Tasks
                 .Where(t => t.Date >= thisWeekbegin && t.Date <= nextWeekend)
                 .OrderBy(t => t.Assigns.Count);
-            return View("_RecentTasks",tasks);
+            return View("_RecentTasks", tasks);
         }
 
         #endregion
@@ -51,7 +73,7 @@ namespace TaskAssignment.Controllers
         public ActionResult SubstationList() {
             var ctx = new TaskAssignmentModel();
             var list = ctx.Substations.DefaultIfEmpty();
-            return View("_SubstationList",list);
+            return View("_SubstationList", list);
         }
 
         public ActionResult AllSubstations() {
---
+++ TaskAssignment/Models/AddTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TaskAssignment.Persistence;
+
+namespace TaskAssignment.Models
+{
+    public class AddTask
+    {
+        public Task Task { get; set; }
+        public int LeaderId { get; set; }
+        public int[] MemberId { get; set; }
+    }
+}
