9bbde570436d362841c3b3f3b1a6f482adc83dd9
TscCai <caijiran@hotmail.com>
2018-02-04 17:23:28 +0800

    Test Excel export

---
+++ TaskAssignment.Tests/OfficeInteropTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace TaskAssignment.Tests
+{
+    [TestClass]
+    public class OfficeInteropTest
+    {
+        [TestMethod]
+        public void TestMethod1() {
+            var app = new Excel.Application();
+            app.Visible = false;
+            try {
+                app.Workbooks.Open("D:\\test.xlsx");
+                var worksheets = app.Worksheets;
+                if (worksheets.Count >= 1) {
+                    // Sheet 1 start as index 1
+                    Excel.Worksheet activeSheet = worksheets[1];
+                    activeSheet.Activate();
+                    
+                    // The inserted row will now be Row 2
+                    activeSheet.Rows[2].Insert(Excel.XlInsertShiftDirection.xlShiftDown);
+
+                    // Delete cols from the tail to aviod the col index change after delete.
+                    activeSheet.Columns[5].Delete(Excel.XlDeleteShiftDirection.xlShiftToLeft);
+                    activeSheet.Columns[4].Delete(Excel.XlDeleteShiftDirection.xlShiftToLeft);
+                   
+                    // Write something in the inserted row
+                    activeSheet.Cells[2, 1] = 5;
+                    activeSheet.Cells[2, 3] = (int)activeSheet.Cells[2, 1].Value + 1;
+                    activeSheet.Cells[2, 2] = "test insert";
+
+                    string filename = "D:\\changed_" + DateTime.Now.ToString("MMddHHmmss") + ".xlsx";
+                    app.ActiveWorkbook.SaveAs(filename);
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+            finally {
+                //app.Workbooks.Close();
+                app.Quit();
+                
+            }
+            Console.WriteLine("Done");
+
+
+
+        }
+    }
+}
---
+++ TaskAssignment/Util/ExcelHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TaskAssignment.Persistence;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+
+namespace TaskAssignment.Util
+{
+    public class ExcelHelper
+    {
+        /// <summary>
+        /// Export to xlsx file using Micorsoft.Office.Interop.Excel
+        /// </summary>
+        /// <param name="attendanceTime"></param>
+        /// <param name="members"></param>
+        /// <param name="record"></param>
+        /// <param name="template"></param>
+        /// <returns></returns>
+        public static string Export(DateTime attendanceTime, IEnumerable<Member> members, IEnumerable<Attendance> record, int[] holidays, int[] extraWorkdays,string template) {
+            string filename = AppDomain.CurrentDomain.BaseDirectory + "\\考勤记录-" + attendanceTime.ToString("yyyy-MM-dd") + ".xlsx";
+            if (File.Exists(filename)) {
+                File.Delete(filename);
+            }
+            var app = new Application();
+            app.Visible = false;
+            try {
+                app.Workbooks.Open(template);
+                // Sheet 1 start as index 1
+                Worksheet activeSheet = app.Worksheets[1];
+                activeSheet.Activate();
+                // TODO:
+                // Delete the redundant date cols acorrding to attandanceTime
+                // Fill in the absent type header, create a dictionary<typeId, col_index>
+                // Fill in names, create a dictionary<memberId, row_index>
+                // Mark all workdays as "on work" for all members, including the extra workDays but excluding the holidays
+                // Read record, fill the table, update the work and absence details in memory
+                // Fill in the work & absence days count
+            }
+            catch (Exception ex) {
+
+            }
+            finally {
+                app.Workbooks.Close();
+                app.Quit();
+            }
+            return "";
+        }
+    }
+}
